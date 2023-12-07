6ecfb1e27c049417fac044e39a3a9d1576d12c40
kwinkunks <kwinkunks@gmail.com>
2023-12-07 20:36:15 +0100

    d7 part 2

--- day7.py
+++ day7.py
@@ -1,53 +1,83 @@
 from pathlib import Path
+from collections import Counter
 
 text = Path('./data/7.txt').read_text()
 data = [row.split() for row in text.split('\n')]
-hands = [row[0] for row in data]
-bids = list(map(int, (row[1] for row in data)))
-game = {h:b for h, b in zip(hands, bids)}
-
-# Group, then sort, then regroup.
-from collections import Counter
-
-# Rely on order, low to high type.
-groups = {}
-for type_ in ['h', 2, 't', 3, 'f', 4, 5]:
-    groups[type_] = list()
+hands = {a: int(b) for a, b in data}
 
 # Score hands.
-for hand in hands:
-    c = Counter(hand)
-    if max(c.values()) == 5:
-        groups[5].append(hand)
-    elif max(c.values()) == 4:
-        groups[4].append(hand)
-    elif max(c.values()) == 3:
-        if min(c.values()) == 2:
-            groups['f'].append(hand)
-        else:
-            groups[3].append(hand)
-    elif max(c.values()) == 2:
-        if len(set(hand)) == 3:
-            groups['t'].append(hand)
-        else:
-            groups[2].append(hand)
-    else:
-        groups['h'].append(hand)
+def get_type(hand):
+    counts = Counter(hand).values()
+    g = str(max(counts))
+    if max(counts) == 3 and min(counts) == 2:
+        g = 'f'  # Full house.
+    if max(counts) == 2 and len(set(hand)) == 3:
+        g = 't'  # Two pair.
+    return g
 
-# Ordering function.
-def strength(hand):
-    strengths = "AKQJT98765432"
-    return tuple(strengths.find(c) for c in hand)
+# Rely on order, low to high type.
+TYPES = '12t3f45'
+def typeset(hands):
+    typesets = {t: list() for t in TYPES}
+    for hand in hands:
+        typesets[get_type(hand)].append(hand)
+    return typesets
+
+# Ordering closure to use as sort key.
+STRENGTHS = "AKQJT98765432"
+def strength(wild=''):
+    def f(hand):
+        strengths = STRENGTHS.replace(wild, '') + wild
+        return tuple(strengths.find(c) for c in hand)
+    return f
 
 # Rank.
-all_hands = []
-for _, group in groups.items():
-    all_hands.extend(sorted(group, key=strength, reverse=True))
+def rank(typesets, wild=''):
+    all_hands = []
+    for _, group in typesets.items():
+        sort = sorted(group, key=strength(wild), reverse=True)
+        all_hands.extend(sort)
+    return all_hands
 
 # Count winnings.
 winnings = 0
-for i, hand in enumerate(all_hands):
-    winnings += (i + 1) * game[hand]
+for i, hand in enumerate(rank(typeset(hands))):
+    winnings += (i + 1) * hands[hand]
 
 # Part 1.
+print(winnings)
+
+# Part 2.
+def best_permutation(hand, wild=''):
+    perms = []
+    for s in STRENGTHS.replace(wild, ''):
+        perms.append(hand.replace(wild, s, 1))
+    return rank(typeset(perms))[-1]
+
+new_hands = {}
+for hand, bid in hands.items():
+    hand_ = hand
+    for _ in range(hand.count('J')):
+        hand_ = best_permutation(hand_, wild='J')
+    new_hands[hand] = hand_ 
+    # Annoying but has to be this way around.
+
+# Well, this is horrific.
+all_hands = []
+wild_hands = new_hands.values()
+for _, group in typeset(wild_hands).items():
+    true_hands = []
+    for wild_hand in group:  # Oh...
+        for hand, hand_ in new_hands.items():  # my...
+            if wild_hand == hand_:  # god...
+                true_hands.append(hand)
+                new_hands.pop(hand)  # sob.
+                break
+    sort = sorted(true_hands, key=strength(wild='J'), reverse=True)
+    all_hands.extend(sort)
+
+winnings = 0
+for i, hand in enumerate(all_hands):
+    winnings += (i + 1) * hands[hand]
+
 print(winnings)
